1cd9debf3916b88ccb87892447f9f6bd61018ce4
Bak <67918140+3skm4L@users.noreply.github.com>
2021-11-16 11:59:55 -0600

    task-36 done (#84)
    Co-authored-by: Baktygul <baliev@my.stlcc.edu>

---
+++ src/main/java/cooksys/projectmanagementapp/projectmanagement/dtos/ProjectDto.java
@@ -0,0 +1,19 @@
+package cooksys.projectmanagementapp.projectmanagement.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProjectDto {
+	private long id;
+
+    private String name;
+
+    private String description;
+
+    private long teamId;
+
+}
