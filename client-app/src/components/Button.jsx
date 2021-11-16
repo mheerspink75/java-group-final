252eeda695cae34b5d5d19b0f02cc0958c295347
Stephen Sohns <37886718+scsohns@users.noreply.github.com>
2021-11-16 12:38:28 -0600

    consolidated buttons and swapped img

--- client-app/src/components/Button.jsx
+++ client-app/src/components/Button.jsx
@@ -2,6 +2,7 @@ import Navbar from "react-bootstrap/Navbar"
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import React from "react";
+import backImg from "../img/Red-Cross-Mark.png"
 
 
 export const SidebarButton = (props) => {
@@ -16,9 +17,25 @@ export const SidebarButton = (props) => {
 
 }
 
-
 export const Button = (props) => {
 	return (<button onClick={props.onClick}>{props.name}</button>)
 }
 
+const BackButtonStyle = {
+	color: 'red',
+	borderRadius: '50%',
+	width: '40px',
+	height: '40px',
+	backgroundImage: `url(${backImg})`,
+	backgroundSize: '100%',
+	backgroundPosition: 'center',
+	border: '1px solid red',
+	alignItems: 'right',
+	marginLeft: '90%',
+	marginRight: '1%',
+	marginTop: '1%',
+}
 
+export const BackButton = (props) => {
+	return (<button style={BackButtonStyle} onClick={props.onClick}> </button>)
+}
