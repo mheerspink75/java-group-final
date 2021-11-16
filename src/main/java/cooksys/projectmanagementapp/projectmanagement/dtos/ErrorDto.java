a020a83395ac877fd717e4c790e380049474d530
Matt Heerspink <51008045+mheerspink75@users.noreply.github.com>
2021-11-16 11:05:50 -0600

    Server matt h (#81)
    * added .gitignore, Company and Teams
    
    * added CompanyDto, CredentialsDto and UserRequestDto
    
    * added ProjectRepository, TeamRepository and UserRepository
    
    * updated ProjectRepository and TeamRepository
    
    * added ErrorDto

---
+++ src/main/java/cooksys/projectmanagementapp/projectmanagement/dtos/ErrorDto.java
@@ -0,0 +1,14 @@
+package cooksys.projectmanagementapp.projectmanagement.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ErrorDto {
+
+    private String message;
+
+}
