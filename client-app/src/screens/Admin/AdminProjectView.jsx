e71cf7d22c4e6c3e19f6e2a7a5ceabb54bd46da1
Stephen Sohns <37886718+scsohns@users.noreply.github.com>
2021-11-22 06:00:46 -0600

    Feature/admin project view service (#168)
    * Example Backend Call
    
    * added newline to end of AdminService.jsx
    
    * Linked project view with API GET request, implemented edit functionality with PATCH request
    
    * removed unecessary edit
    
    * resolved errors
    
    * removed unecessary imports
    
    * reworked useEffect in project-view to remove warning
    
    * Compiled successfully!
    
    * Update client-app/src/screens/Admin/AdminProjectView.jsx
    
    Co-authored-by: xorps <758027+xorps@users.noreply.github.com>
    
    * Update client-app/src/services/AdminService.jsx
    
    Co-authored-by: xorps <758027+xorps@users.noreply.github.com>
    
    * Update client-app/src/screens/Admin/AdminProjectView.jsx
    
    Co-authored-by: xorps <758027+xorps@users.noreply.github.com>
    
    * Update AdminProjectView.jsx
    
    rename state functions to setXX
    
    Co-authored-by: Seth Klumph <stklumph@crimson.ua.edu>
    Co-authored-by: xorps <758027+xorps@users.noreply.github.com>

--- client-app/src/screens/Admin/AdminProjectView.jsx
+++ client-app/src/screens/Admin/AdminProjectView.jsx
@@ -1,19 +1,25 @@
 import React from "react"
 import { Button } from "../../components/Button"
 import TextArea from "../../components/TextArea"
-import {useState} from "react"
+import { useState, useEffect } from "react"
+import { getProject, updateProject } from "../../services/AdminService"
+import { Link } from "react-router-dom"
+import Logo from "../../components/Logo"
+
 const AdminProjectView = () => {
-	let value = `
-	Generic description text for all the users at FedEx. I will now begin my lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipsicing elit. Sed sapien nulla, venenatis vestibulum bibendum nec, ornare et magna. Generic description text for all the users at FedEx. I will now begin my lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipsicing elit. Sed sapien nulla, venenatis vestibulum bibendum nec, ornare et magna. Generic description text for all the users at FedEx. I will now begin my lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipsicing elit. Sed sapien nulla, venenatis vestibulum	Generic description text for all the users at FedEx. I will now begin my lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipsicing elit. Sed sapien nulla, venenatis vestibulum bibendum nec, ornare et magna. Generic description text for all the users at FedEx. I will now begin my lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipsicing elit. Sed sapien nulla, venenatis vestibulum bibendum nec, ornare et magna. Generic description text for all the users at FedEx. I will now begin my lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipsicing elit. Sed sapien nulla, venenatis vestibulum	
-	`
+
 	const [edit, SetEdit] = useState(false)
 	const editButton = () => {
 		SetEdit(true)
 	}
 	const saveButton = () => {
-		SetEdit(false)
+		const newData = projectData
+		newData["description"] = document.getElementById('projectDescription').value
+		setProjectData(newData)
+		updateProject(projectData["id"], projectData).then(() => SetEdit(false))
 	}
 	const cancelButton = () => {
+		document.getElementById('projectDescription').value = projectData["description"]
 		SetEdit(false)
 	}
 
@@ -21,34 +27,59 @@ const AdminProjectView = () => {
 		width: '80%',
 		height: '500px',
 		margin: 'auto',
-		borderRadius:'20px',
-		padding:'20px',
-		resize:'none'
+		borderRadius: '20px',
+		padding: '20px',
+		resize: 'none',
+		border: '1px solid black'
 	}
+
+	const [projectLoaded, setProjectLoaded] = useState(false)
+	const [projectData, setProjectData] = useState({})
+
+	let projectId = 7 //number to be pulled from previous page
+	
+	useEffect(() => {
+		const loadProjectData = () => {
+			getProject(projectId).then((data) => setProjectData(data)).then( () => setProjectLoaded(true))
+		}
+		loadProjectData()
+	}, [projectId])
+
 	
 
 	return (
 		<div>
-			Insert Logo Component
-			<div style={{ display: 'flex', flexDirection: 'column' }}>
-				<h1 style={{ textAlign: 'center', marginTop: '10%' }}>Project 1</h1>
-				<a href="/admin/teams" style={{ textDecoration: 'none', marginLeft: '10%',marginRight:'84%' }}>&lt;- Back</a>
+			{/* outer layer*/}
+			<Link to='/admin/home'>
+				<Logo></Logo>
+			</Link>
+
+			{!projectLoaded ? <p>Loading Data</p> : <div>
+
+				<div style={{ display: 'flex', flexDirection: 'column' }}>
+					<h1 style={{ textAlign: 'center' }}>{projectData.name}</h1>
+					<Link to="/admin/teams" style={{ textDecoration: 'none', marginLeft: '10%', marginRight: '80%', fontSize: '20px' }}>&lt;- Back</Link>
+				</div>
+
 
 				{edit ? <div style={{ display: 'flex', flexDirection: 'column' }} >
-					<TextArea style={textAreaStyle} content={value} id="projectDescription" readOnly={false} > </TextArea>
+					<TextArea style={textAreaStyle} content={projectData.description} id="projectDescription" readOnly={false} > </TextArea>
 					<div style={{ marginLeft: '70%', paddingTop: '10px', display: 'flex', columnGap: '20px' }}>
 						<Button name="save" onClick={saveButton}  ></Button>
 						<Button name="cancel" onClick={cancelButton} ></Button>
 					</div>
 				</div> :
 					<div style={{ display: 'flex', flexDirection: 'column' }}>
-						<TextArea style={textAreaStyle} content={value} id="projectDescription" readOnly={true} > </TextArea>
+						<TextArea style={textAreaStyle} content={projectData.description} id="projectDescription" readOnly={true} > </TextArea>
 						<div style={{ marginLeft: '80%', paddingTop: '10px' }}>
 							<Button name="edit" onClick={editButton}  ></Button>
 						</div>
 					</div>}
 			</div>
+			}
+			{/* outer layer*/}
 		</div>
+
 	)
 }
 
---
+++ client-app/src/services/AdminService.jsx
@@ -0,0 +1,26 @@
+const getAllProjectsUrl = "http://localhost:8080/api/admin/projects/";
+
+export const getAllProjects = async () => {
+    const response = await fetch(getAllProjectsUrl);
+    const data = await response.json();
+    console.log(data);
+    return data;
+};
+
+export const getProject = async (id) => {
+	const response = await fetch(getAllProjectsUrl +`/${id}`);
+	const data = await response.json();
+	return data;
+};
+
+export const updateProject = async (id, requestBody) => {
+	const response = await fetch(getAllProjectsUrl + `/${id}`, {
+		 method:'PATCH',
+		 headers: {
+			 'Accept': 'application/json',
+			 'Content-Type': 'application/json'
+		 },
+		 body: JSON.stringify(requestBody) 
+	});
+	await response.json()
+};
